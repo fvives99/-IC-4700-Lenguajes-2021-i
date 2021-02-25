75c40dcf566e3351c433729cf1d41d27da640ea1
Fabian Vives <f.vives99@gmail.com>
2021-02-24 20:16:53 -0600

    Comentarios

--- Practicas/operaciones.c
+++ Practicas/operaciones.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
+//Declaracion de variables
 int main(){
 int num1, num2, total;
 printf("Ingrese el primer número: ");
+//Se registra el primer valor
 scanf("%d" ,&num1);
 printf("Ingrese el segundo número: ");
+//Se registra el segundo valor
 scanf("%d" ,&num2);
 total = num1 + num2;
+//Asigna la suma de ambos valores a la variable total
 printf("- La suma de ambos números es %d \n", total);
 total = num1 - num2;
+//Asigna la resta de ambos valores a la variable total
 printf("- La resta de ambos números es %d \n", total);
 total = num1 * num2;
+//Asigna la multiplicacion de ambos valores a la variable total
 printf("- La multiplicación de ambos números es %d \n", total);
 total = num1 / num2;
+//Asigna la devision de ambos valores a la variable total
 printf("- La división de ambos números es %d \n", total);
 return 0;
 }
--- Practicas/pruebas.c
+++ Practicas/pruebas.c
@@ -2,14 +2,16 @@
 #include <stdlib.h>
 #include <time.h>
 int main(){
+// Se declara la variable que se utilizara como contador
 int z=1;
-
+// Se inicia el puntero en NULL para que no utilice basura
 int *ptr = NULL;
 ptr = &z;
 int a[10][10];
 for(int i=0;i<3;i++){
 for(int j=0;j<3;j++){
 a[i][j]=*ptr;
+// Se imprime su el numero de la matriz junto con su direccion de memoria
 printf("\t %d %p",a[i][j],&a[i][j]);
 *ptr+=1;
 }
